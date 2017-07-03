ba8ce6db2afc62b93789f62da0a6e8bae022058b
Arnold Chan <arnoldthchan@gmail.com>
2017-07-03 18:28:20 +0000

    added package.json and gitingore

---
+++ download_avatars.js
@@ -0,0 +1,7 @@
+// Given a GitHub repository name and owner
+// download all the contributors' profile images
+// and save them to a subdirectory, avatars/
+var var1 = process.argv[2];
+var var2 = process.argv[3];
+
+console.log(var1, var2);
