eb896180dad581f938e7c36c44326acaa2944a1b
Arnold Chan <arnoldthchan@gmail.com>
2017-07-03 21:19:39 +0000

    Connected functions together

--- download_avatars.js
+++ download_avatars.js
@@ -2,61 +2,61 @@
 // download all the contributors' profile images
 // and save them to a subdirectory, avatars
 
-// var var1 = process.argv[2];
-// var var2 = process.argv[3];
-
-// console.log(var1, var2);
-
 var request = require('request');
 var fs = require('fs');
 
-
-console.log('Welcome to the GitHub Avatar Downloader!\n');
+var owner = process.argv[2];
+var repo = process.argv[3];
+var folders = './avatars';
 
 var GITHUB_USER = 'arnoldthchan';
 var GITHUB_TOKEN = 'b56d31ba5c5cb58f1f0a55a2f9d9cf22877f0603';
+// var API_KEY = process.env.github_API_KEY;
 
-function complete(){
-  console.log('Download completed')
-}
 function downloadImageByURL(url, filePath) {
   request.get(url)
          .on('error', function (err) {
            throw err;
          })
-         .on('response', function (response) {
-           console.log('Response Status Code: ', response.statusCode, '\nResponse Content Type:', response.headers['content-type']);
-         })
-         .pipe(fs.createWriteStream(filePath + '.jpg').on('finish', complete));
+         // .on('response', function (response) {
+         //   console.log('downloadImageByURL-Response Status Code: ', response.statusCode, '\nResponse Content Type:', response.headers['content-type']);
+         // })
+         .pipe(fs.createWriteStream(filePath + '.jpg'));
 }
 
-//Fetches contributors as a JSON list
-//Involves a callback function
-//to loop and print out avatar URLs, for each object
-function getRepoContributors(repoOwner, repoName, callback) {
+function iterateContributors(text){
+  for (i in text){
+    downloadImageByURL(text[i].avatar_url, `avatars/${text[i].login}`);
+    }
+}
+//Requests using URL with user-agent and github user + token
+function getRepoContributors(repoOwner, repoName, cb) {
 var options = {
         url: 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
         headers: {
             'User-Agent': 'GitHub Avatar Downloader - Student Project'
         }
     };
-
+//Then sends JSON pased body into callback function
 request(options, function(err, res, body){
   if (err) throw err;
-  console.log('Response Status Code:', res.statusCode);
+  // console.log('request-Response Status Code:', res.statusCode);
   var text = JSON.parse(body);
-  for (i in text){
-    console.log(text[i].avatar_url);
-    console.log(`avatars/${text[i].login}`)
-  }
+    cb(text);
   });
 }
 
-//https://github.com/nodejs/node
-//https://github.com/jquery/jquery
-getRepoContributors('jquery', 'jquery', function(err, result) {
-  console.log('Errors:', err);
-  console.log('Result:', result);
-});
 
-downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "avatars/kvirani")
+//START
+getRepoContributors(owner, repo, iterateContributors);
+
+console.log('\nWelcome to the GitHub Avatar Downloader!\n');
+console.log(`Attempting download of ${repo} owned by ${owner}`);
+
+//Checks if folder with foldername exist
+//If not, creates one
+function createFolder(folders){
+  if (!fs.existsSync(folders)){
+    fs.mkdirSync(folders);
+  }
+}
