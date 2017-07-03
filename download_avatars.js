a06ba01c1d326fbe67fd7b4ef026458187115e53
Arnold Chan <arnoldthchan@gmail.com>
2017-07-03 18:33:01 +0000

    Added request module and empty function

--- download_avatars.js
+++ download_avatars.js
@@ -1,7 +1,20 @@
 // Given a GitHub repository name and owner
 // download all the contributors' profile images
-// and save them to a subdirectory, avatars/
-var var1 = process.argv[2];
-var var2 = process.argv[3];
+// and save them to a subdirectory, avatars
 
-console.log(var1, var2);
+// var var1 = process.argv[2];
+// var var2 = process.argv[3];
+
+// console.log(var1, var2);
+
+var request = require('request');
+
+console.log('Welcome to the GitHub Avatar Downloader!');
+
+//Fetches contributors as a JSON list
+//Involves a callback function
+//to loop and print out avatar URLs, for each object
+function getRepoContributors(){
+
+  console.log('avatar_url');
+}
